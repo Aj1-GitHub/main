021e50ca7bde4ff05022a1b1e8ce962cdc8d95c3
Aj1-GitHub <121291881+Aj1-GitHub@users.noreply.github.com>
2023-02-24 12:06:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ else
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP Application - AWS Elastic Beanstalk</title>
+    <title>PHP Application - AWS Elastic Beanstalk2</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
@@ -22,10 +22,10 @@ else
     <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
 <body>
-    <section class="congratulations">
+    <section class="DEMO2">
         <h1>Congratulations!</h1>
-        <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP version <?= phpversion() ?></p>
+        <p>Your AWS IS HERE FOR YOU KIND INFOR AJAYA  <em>PHP</em> applAJY IS THE ONE WHO IS PRESENTING THIS DEMO TO YOU AWS&nbsp;Cloud</p>
+        <p>You are running PHP version5 <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
     </section>
 
